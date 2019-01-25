fbb5904549a2cdae43f4900b5353baf9583fc797
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2019-01-25 12:06:01 +0300

    update

--- chat_client/src/main.rs
+++ chat_client/src/main.rs
@@ -1,23 +1,58 @@
-use std::{io, thread};
-use std::io::Write;
+use std::{io, thread, error};
+use std::io::{Write, BufReader, BufRead};
+use std::time::Duration;
 use std::net::TcpStream;
+use std::sync::mpsc;
 
-fn main() -> io::Result<()> {
-    thread::spawn(|| {
-        loop {
-            thread::sleep_ms(3000);
-            //println!("\rHi world!");
+fn handle_connection(rx: mpsc::Receiver<String>) -> Result<(), Box<dyn error::Error>> {
+    let stream = TcpStream::connect("127.0.0.1:8080")?;
+    stream.set_read_timeout(Some(Duration::from_millis(100)))?;
+    let mut stream = BufReader::new(stream);
+    let mut line = String::new();
+
+    loop {
+        let res = stream.read_line(&mut line);
+        match res {
+            Ok(0) => {
+                println!("server connection closed");
+                return Ok(());
+            },
+            Ok(_) => (),
+            Err(err) => {
+                if err.kind() == io::ErrorKind::WouldBlock {
+                    let msg = match rx.try_recv() {
+                        Ok(msg) => msg,
+                        Err(mpsc::TryRecvError::Empty) => continue,
+                        Err(err) => return Err(Box::new(err)),
+                    };
+                    stream.get_mut().write_all(msg.as_bytes())?;
+                    continue;
+                } else {
+                    return Err(Box::new(err));
+                }
+            },
+        };
+        print!("\r{}", line);
+        print!("> ");
+        io::stdout().flush()?;
+    }
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let mut buf = String::new();
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        if let Err(err) = handle_connection(rx) {
+            eprintln!("Error: {:?}", err);
         }
     });
-    let mut buf = String::new();
-    let mut conn = TcpStream::connect("127.0.0.1:8080")?;
     loop {
-        io::stdin().read_line(&mut buf)?;
         print!("> ");
-        io::stdout().flush();
-        conn.write_all(buf.as_bytes())?;
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut buf)?;
 
-        // message
+        tx.send(buf.clone())?;
+
         buf.clear();
     }
 }
--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,37 +1,63 @@
 use std::net::{TcpListener, TcpStream};
 use std::{io, thread};
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Write};
 use std::time::Duration;
 use std::sync::{Mutex, Arc};
 
 type History = Arc<Mutex<Vec<String>>>;
+
+fn send_history(history: &[String], pos: &mut usize, stream: &mut TcpStream) -> io::Result<()> {
+    if *pos < history.len() {
+        for msg in &history[*pos..] {
+            stream.write_all(msg.as_bytes())?;
+        }
+        *pos = history.len();
+    }
+    Ok(())
+}
 
 fn handle_client(stream: io::Result<TcpStream>, history: History) -> io::Result<()> {
     let stream = stream?;
     let addr = stream.peer_addr();
     println!("got connection: {:?}", addr);
     stream.set_read_timeout(Some(Duration::from_millis(100)))?;
-    let stream = BufReader::new(stream);
-    let mut pos = 0;
+    let mut stream = BufReader::new(stream);
+    let mut pos = {
+        let n = history.lock().unwrap().len();
+        if n < 10 {
+            0
+        } else {
+            n - 10
+        }
+    };
 
-    for line in stream.lines() {
-        let line = match line {
-            Ok(l) => l,
+    loop {
+        let mut line = String::new();
+        let res = stream.read_line(&mut line);
+        match res {
+            Ok(0) => {
+                println!("connection closed: {:?}", addr);
+                return Ok(());
+            },
+            Ok(_) => (),
             Err(err) => {
                 if err.kind() == io::ErrorKind::WouldBlock {
+                    let history_lock = history.lock().unwrap();
+                    send_history(&history_lock, &mut pos, stream.get_mut())?;
                     continue;
                 } else {
                     return Err(err);
                 }
             },
         };
+
         let mut history_lock = history.lock().unwrap();
+        send_history(&history_lock, &mut pos, stream.get_mut())?;
         println!("client wrote: [{}] {:?}",
             history_lock.len(), line);
         history_lock.push(line);
+        pos += 1;
     }
-    println!("connection closed: {:?}", addr);
-    Ok(())
 }
 
 fn main()  -> io::Result<()> {
